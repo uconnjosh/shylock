b6f5cee696eb424d7ebbf50d2782fae7261cb7f3
Josh Paul <jpaul@focusvision.com>
2017-04-27 01:12:19 -0700

    Add update support for Account.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,11 +1,19 @@
 class AccountsController < ApplicationController
   def create
-    Account.create(account_create_params)
+    Account.create(account_params)
+  end
+
+  def update
+    Account.find(account_id).update_attributes(account_params)
   end
 
 private
 
-  def account_create_params
+  def account_params
     ActiveModelSerializers::Deserialization.jsonapi_parse(params, only: [:email, :"credit-limit", :"open-date", :apr ])
+  end
+
+  def account_id
+    params[:id]
   end
 end
