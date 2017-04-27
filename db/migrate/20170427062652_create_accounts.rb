8805f2949e6422e68ea09db13f181db4d27987bc
Josh Paul <jpaul@focusvision.com>
2017-04-26 23:28:17 -0700

    Setup new Rails API application. Include the pg gem and configure for use with Postgres. Generate a model for account.

---
+++ db/migrate/20170427062652_create_accounts.rb
@@ -0,0 +1,13 @@
+class CreateAccounts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :accounts do |t|
+      t.string :email
+      t.string :password
+      t.string :apr
+      t.string :credit_limit
+      t.string :open_date
+
+      t.timestamps
+    end
+  end
+end
