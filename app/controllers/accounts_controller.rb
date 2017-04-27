bcb8defa0b927b7f2b2e06b5dce33d45c4fb84e4
Josh Paul <jpaul@focusvision.com>
2017-04-27 00:53:22 -0700

    Includes ability to create a new account throught the apigit status (with strong params and deserialization).

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,11 @@
 class AccountsController < ApplicationController
   def create
+    Account.create(account_create_params)
+  end
+
+private
+
+  def account_create_params
+    ActiveModelSerializers::Deserialization.jsonapi_parse(params, only: [:email, :"credit-limit", :"open-date", :apr ])
   end
 end
