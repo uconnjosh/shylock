ab75048e480cab76f645b0b42e695628c896e1b2
Josh Paul <jpaul@focusvision.com>
2017-04-27 23:25:00 -0700

    Includes business logic on Account for discovering balance (based on day), average daily balance, and total statement interest for a given set of time (defaults to 30 days).

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,21 @@
 class Account < ApplicationRecord
   has_many :transactions
+
+  def balance(day = DateTime.now)
+    transactions.where("created_at <= :day", day: day).pluck(:amount).inject { |a, e| a.to_f + e.to_f }.to_f
+  end
+
+  def statement_interest(days = 30)
+    average_daily_balance * apr.to_f/365 * days
+  end
+
+  def average_daily_balance(days = 30)
+    summed_balance = 0
+
+    days.times do |i|
+      summed_balance += balance(i.days.ago)
+    end
+
+    summed_balance / days
+  end
 end
