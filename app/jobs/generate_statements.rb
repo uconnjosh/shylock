a752f740e404bea111e49c8accafd47ca57cda58
Josh Paul <jpaul@focusvision.com>
2017-05-02 10:31:37 -0700

    Use method on Account for discovering ids where statement due.

--- app/jobs/generate_statements.rb
+++ app/jobs/generate_statements.rb
@@ -8,27 +8,8 @@ class GenerateStatements
     generate_statements
   end
 
-  def account_ids
-  	query <<-SQL
-  	  SELECT a.id
-      FROM accounts a
-      JOIN statements s1 on (a.id = s1.account_id)
-      LEFT OUTER JOIN statements s2 on (a.id = s2.account_id AND (s1.created_at < s2.created_at OR s1.created_at = s2.created_at AND s1.id < s2.id))
-      WHERE s2.id IS NULL;
-    SQL
-
-  	# Account.where("statements.last.created_at >= :date", date: DateTime.now - 30.days)
-   #  query = <<-SQL
-   #    accounts.statements.last
-
-   #  Statement.where(
-   #    'created_at >= :day',
-   #    day: DateTime.now - 30.days
-   #  ).pluck(:account_id).uniq
-  end
-
   def genererate_statements
-    account_ids.each do |a_id|
+    Account.ids_where_statement_due.each do |a_id|
       @account = Account.find(a_id)
       generate_statement
     end
