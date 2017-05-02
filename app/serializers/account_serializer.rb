d3ad3082065fcaaf9062dc2db343acdc78eaf54b
Josh Paul <jpaul@focusvision.com>
2017-05-02 10:12:12 -0700

    Include balance in account serialization.

--- app/serializers/account_serializer.rb
+++ app/serializers/account_serializer.rb
@@ -1,5 +1,5 @@
 class AccountSerializer < ActiveModel::Serializer
-  attributes :apr, :credit_limit, :open_date, :user_id
+  attributes :apr, :credit_limit, :open_date, :user_id, :balance
 
   has_many :statements
   has_many :transactions
